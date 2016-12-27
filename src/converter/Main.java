159330494cda142c337ab67fdd3aa7d69fd24038
Samuel Wilson <samWson@users.noreply.github.com>
2016-12-27 19:36:33 +1300

    Add command line help

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -3,6 +3,18 @@ package converter;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello");
+
+	// Command line help message.
+	final String help = new StringBuilder( "Usage: converter [-options] [args...]\n")
+	    .append("(to convert a value)\n")
+	    .append("where options include:\n")
+	    .append("\t-mph\tto convert from kilometers per hour to miles per hour\n")
+	    .append("\t-kph\tto convert from miles per hour to kilometers per hour\n")
+	    .toString();
+	
+	
+	if (args.length == 0) {
+	    System.out.println(help);
+	}
     }
 }
