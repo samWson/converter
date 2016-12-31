6d22feca0f56093576d279e2d5ea63d967e2898b
Samuel Wilson <samWson@users.noreply.github.com>
2016-12-31 17:44:58 +1300

    Comment out methods and constants, which are in Velocity class

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -2,29 +2,29 @@ package converter;
 
 public class Main {
 
-    // The number of miles in a kilometer.
-    private static final double MILES_IN_KILOMETER = 1.6026;
+    // // The number of miles in a kilometer.
+    // private static final double MILES_IN_KILOMETER = 1.6026;
 
-    // The number of kilometers in a mile.
-    private static final double KILOMETERS_IN_MILE = 0.624;
+    // // The number of kilometers in a mile.
+    // private static final double KILOMETERS_IN_MILE = 0.624;
 
-    /**
-       Convert miles per hour to kilometers per hour.
-       @param mph the mile per hour value to be converted
-       @return the converted value
-    */
-    private static double mphToKph(double mph) {
-	return mph * MILES_IN_KILOMETER;
-    }
+    // /**
+    //    Convert miles per hour to kilometers per hour.
+    //    @param mph the mile per hour value to be converted
+    //    @return the converted value
+    // */
+    // private static double mphToKph(double mph) {
+    // 	return mph * MILES_IN_KILOMETER;
+    // }
 	
-    /**
-       Convert kilometers per hour to miles per hour.
-       @param kph the kilometer per hour vaule to be converted
-       @return the converted value
-    */
-    private static double kphToMph(double kph) {
-	return kph * KILOMETERS_IN_MILE;
-    }
+    // /**
+    //    Convert kilometers per hour to miles per hour.
+    //    @param kph the kilometer per hour vaule to be converted
+    //    @return the converted value
+    // */
+    // private static double kphToMph(double kph) {
+    // 	return kph * KILOMETERS_IN_MILE;
+    // }
 	
     /**
        The entry point of the program. Selects the appropriate action based
