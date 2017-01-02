40557493cf7d25a5c0d0d4cba76ed05a5b837d68
Samuel Wilson <samWson@users.noreply.github.com>
2017-01-02 19:57:13 +1300

    Add VelocityTester class

--- src/test/java/converter/VelocityTester.java
+++ src/test/java/converter/VelocityTester.java
@@ -1,15 +1,22 @@
 package converter.tests;
 
-import converter.Velocity;
+//import converter.Velocity;
 
 /**
    Contains unit tests for the converter.Velocity class.
 */
 public final class VelocityTester {
 
+    /**
+       Runs the unit tests.
+       @param args the command line arguments
+    */
+    public static void main(String[] args) {
+	
     // Testing the static members.
-    assert Velocity.MILES_IN_KILOMETER == 1.6026;
-    assert Velocity.KILOMETERS_IN_MILE == 0.624;
-
+	//  assert Velocity.MILES_IN_KILOMETER == 1.6026 : "MILES_IN_KILOMETER is not 1.6026";
+	//    assert Velocity.KILOMETERS_IN_MILE == 0.624 : "KILOMETERS_IN_MILE is not 0.624";
+	
+    }
     
 }
