5233db9e4423a1a536a688b800371b3a870ae2f6
Samuel Wilson <samWson@users.noreply.github.com>
2017-01-02 18:19:17 +1300

    Change directory structure to match Gradle recomendations

---
+++ src/test/java/converter/VelocityTester.java
@@ -0,0 +1,15 @@
+package converter.tests;
+
+import converter.Velocity;
+
+/**
+   Contains unit tests for the converter.Velocity class.
+*/
+public final class VelocityTester {
+
+    // Testing the static members.
+    assert Velocity.MILES_IN_KILOMETER == 1.6026;
+    assert Velocity.KILOMETERS_IN_MILE == 0.624;
+
+    
+}
